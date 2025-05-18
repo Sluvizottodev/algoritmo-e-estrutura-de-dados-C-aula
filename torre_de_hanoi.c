754fb90fc5b60c402722b8d88099394e0e100d8f
SLuvizotto <luvizottodev@gmail.com>
2025-05-17 21:24:53 -0300

    up-> explicação de exercício T. Hanoi

--- torre_de_hanoi.c
+++ torre_de_hanoi.c
@@ -17,3 +17,21 @@ int main(){
     moveDisks(n, 'A', 'C', 'B');
     return 0;
 }
+/*
+Desafio da Torre de Hanoi!
+São 2 regras:
+  1. Você só pode mover um disco por vez.
+  2. Um disco maior nunca pode ser colocado em cima de um disco menor.
+
+-Essa função principal move n discos da torre source para a torre dest, usando a torre aux como apoio.
+
+# Funcionamento:
+Etapa 1: A -> C
+Etapa 2: A -> B
+Etapa 3: C -> B
+Etapa 4: A -> C
+Etapa 5: B -> A
+Etapa 6: B -> C
+Etapa 7: A -> C
+
+*/
