2f35080a1f6efabb981cfe8ccdb35c8b31654af7
SLuvizotto <luvizottodev@gmail.com>
2025-07-07 14:08:47 -0300

    feat: adiciona estrutura de dados para fila em questao_01.c

---
+++ questoes_provas_antigas/2024.2 - P2/questao_01.c
@@ -0,0 +1,9 @@
+struct no_fila {
+    int valor;
+    struct no_fila *proximo;    
+};
+
+/* Estrutura encadeada onde cada nó aponta para o próximo
+int valor -> guarda a informação / campo de dado
+struct no_fila *proximo -> aponta para o prócimo nó
+*/
