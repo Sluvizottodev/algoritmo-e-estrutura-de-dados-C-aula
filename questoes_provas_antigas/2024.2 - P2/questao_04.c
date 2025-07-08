429d88650e18f25bb60ef7da45cab6e248d31042
SLuvizotto <luvizottodev@gmail.com>
2025-07-08 15:11:59 -0300

    feat: adiciona função para excluir elemento da fila em questao_04.c

---
+++ questoes_provas_antigas/2024.2 - P2/questao_04.c
@@ -0,0 +1,9 @@
+int excluir_fila(No* sentinela){
+    No* primeiro = sentinela->proximo; 
+    if(primeiro == NULL){
+        return 0;
+    }
+    sentinela->proximo = primeiro->proximo;
+    free(primeiro);
+    return 1;
+}
